f478d01606b5ed73440e64312f739722689e2ea0
mothy900 <mothy900@naver.com>
2022-12-22 17:42:46 +0900

    feat - api movie get Info

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import NavBar from "../components/NavBar";
+import Seo from "../components/Seo";
+
+export default function About() {
+  return (
+    <div>
+      <Seo title={"About"} />
+      <h1>about</h1>
+    </div>
+  );
+}
